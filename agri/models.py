5740f5e1ecde1e9a4c19d5df70334e00504e1f0a
MKhajababa <khajababa.md2002@gmail.com>
2021-12-18 18:58:38 +0530

    hhh

--- agri/models.py
+++ agri/models.py
@@ -6,25 +6,27 @@ from django.db.models.deletion import CASCADE
 from django.db.models.fields import EmailField
 from django.db.models.fields.related import ForeignKey, OneToOneField
 from phonenumber_field.modelfields import PhoneNumberField
-
-# class User(AbstractBaseUser):
-#     fullname = models.CharField(max_length=30,unique=True)
-#     phone = PhoneNumberField(default = None)
-#     is_farmer = models.BooleanField(default=False)
-#     is_Employee = models.BooleanField(default=False)
-#     is_private = models.BooleanField(default = False)
+#Create your models here.
+class AgriUser(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    phone = PhoneNumberField(default = None)
+    is_farmer = models.BooleanField(default=False)
+    is_Employee = models.BooleanField(default=False)
+    is_private = models.BooleanField(default = False)
+     
 
 
 
 
 class FarmerUser(models.Model):   
-    user = OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    user = models.OneToOneField(AgriUser,on_delete=models.CASCADE,primary_key=True)
     adhaar = models.CharField(max_length=20,unique=True)
     village = models.CharField(max_length=20)
     district = models.CharField(max_length=20)
     state = models.CharField(max_length=20)
 
 class GovempUser(models.Model):
-    user = OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
-    Email = EmailField(unique=True)
+    user = models.OneToOneField(AgriUser,on_delete=models.CASCADE,primary_key=True)
+    Email = EmailField(unique=True,default=None)
+   
     Address = models.CharField(max_length=100)
