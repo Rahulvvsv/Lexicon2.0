0f6e218d3ef9c31d626453bb80cd7c0009dd365e
rahul <vup@gmail.com>
2021-12-18 18:59:29 +0530

    helo there

--- agri/views.py
+++ agri/views.py
@@ -6,15 +6,11 @@ def index(request):
 
 
 def login(request):
-<<<<<<< HEAD
     if request.method == "POST":
         username = request.POST['uname']
         password = request.POST['pass']
         
     return render(request,"agri/login.html")
-=======
-    return render(request,"agri/login.html")
 
 def maps(request):
-    return render(request,"agri/gmaps.html")
->>>>>>> 0b5a94eea46549a41a36552c697e847933852c6a
+    return render(request,'agri/gmaps.html')
