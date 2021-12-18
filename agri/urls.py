e798f8a097c2175b08b4ca54aea774b16ff39833
MKhajababa <khajababa.md2002@gmail.com>
2021-12-18 23:19:19 +0530

    'hhh'

--- agri/urls.py
+++ agri/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('',views.index),
     path("login",views.login,name='login'),
     path("maps",views.maps,name='maps'),
+    path("weather",views.weatherFore,name = 'weather')
 ]
