c25c366bd1d262a6bd0aa3dfae497d31a0cd2c79
MKhajababa <khajababa.md2002@gmail.com>
2021-12-18 17:17:27 +0530

    second commit

--- agri/models.py
+++ agri/models.py
@@ -1,3 +1,30 @@
-from django.db import models
 
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,User
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import EmailField
+from django.db.models.fields.related import ForeignKey, OneToOneField
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+# class User(AbstractBaseUser):
+#     fullname = models.CharField(max_length=30,unique=True)
+#     phone = PhoneNumberField(default = None)
+#     is_farmer = models.BooleanField(default=False)
+#     is_Employee = models.BooleanField(default=False)
+#     is_private = models.BooleanField(default = False)
+
+
+
+
+class FarmerUser(models.Model):   
+    user = OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    adhaar = models.CharField(max_length=20,unique=True)
+    village = models.CharField(max_length=20)
+    district = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+
+class GovempUser(models.Model):
+    user = OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    Email = EmailField(unique=True)
+    Address = models.CharField(max_length=100)
