b7293fbc3c1c2698c8e70a6f2ec3f8fa4ed28a18
rajesh <rajesh@gmail.com>
2021-12-19 07:07:53 +0530

    map"

--- agri/urls.py
+++ agri/urls.py
@@ -4,9 +4,9 @@ urlpatterns = [
     path('',views.index),
     path("login",views.loggin,name='login'),
     path("maps",views.maps,name='maps'),
-   
+    path("scrape",views.scrape,name='srape'),
     path("weather",views.weatherFore,name = 'weather'),
-    
+    path("scrape_post",views.scrape_post,name='srape_post'),
     path("log",views.logingout,name="logout"),
     path("dtail/<int:id>",views.dtail,name="dtail")
 ]
