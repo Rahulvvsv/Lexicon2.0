248d7ca00a91c9e808ed675010c1c7def218549a
MKhajababa <khajababa.md2002@gmail.com>
2021-12-19 06:29:54 +0530

    'hh'

--- agri/urls.py
+++ agri/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index),
-    path("login",views.login,name='login'),
+    path("login",views.loggin,name='login'),
     path("maps",views.maps,name='maps'),
-    path("scrape",views.scrape,name="scrape"),
+   
     path("weather",views.weatherFore,name = 'weather'),
-    path("scrape_post",views.scrape_post,name="scrape_post")
+    
+    path("log",views.logingout,name="logout"),
+    path("dtail/<int:id>",views.dtail,name="dtail")
 ]
