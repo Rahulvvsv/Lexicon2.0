d6ffba9fed997c88e873c8e54f791158d9399b88
MKhajababa <khajababa.md2002@gmail.com>
2021-12-19 12:01:33 +0530

    khahs

--- agriculture/urls.py
+++ agriculture/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('agri.urls')),
+    (r'^search/', include('googlesearch.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
